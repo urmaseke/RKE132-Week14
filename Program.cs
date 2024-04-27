0008de666e6cb45e7c13232f33eaf82cabf9045e
Urmas Õunapu. <urmas.ounapuu@tktk.ee>
2024-04-27 05:42:41 +0300

    add week14 code

---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿Console.WriteLine("Name your dog:");
+string myDogName = Console.ReadLine();
+
+
+Dog myDog = new Dog(myDogName);
+//myDog._name = "Good boy";
+
+Dog neighboursDog = new Dog("Good girl");
+//neighboursDog._name = "Good girl";
+
+Console.WriteLine($"My dogs name is {myDog.Name}.");
+Console.WriteLine($"My neighbours dogs name is {neighboursDog.Name}.");
+
+myDog.Rename("Bad Boy");
+
+while (myDog.LevelOfHappiness!= 5)
+{
+    myDog.Bark();
+}
+  myDog.Wagtail();
+
+
+class Dog
+{
+    private string _name;
+    private int _levelOfHappiness;
+
+    public Dog(string name)
+    {
+        _name = name;
+        _levelOfHappiness = 0;
+    }
+    public string Name
+    {
+        get { return _name; }
+    }
+    public int LevelOfHappiness
+    {
+        get { return _levelOfHappiness; }
+    }
+    public void Rename(string newName)
+    {
+        _name = newName;
+        Console.WriteLine($"The dog has been renamed to {newName}");
+    }
+    public void Bark()
+    {
+        Console.WriteLine("Woof-woof!");
+        _levelOfHappiness++;
+    }
+
+    public void Wagtail()
+    {
+        Console.WriteLine("Wiggle-wiggle!");
+    }
+}
